6d1b30a00182a5cb6cda7343aa2d599ae399a38b
Noah Jutz <noahjutz@tutanota.de>
2021-01-05 19:34:00 +0100

    Use app icon in topBar in HomeScreen

--- app/src/main/java/com/noahjutz/findchip/ui/home/HomeScreen.kt
+++ app/src/main/java/com/noahjutz/findchip/ui/home/HomeScreen.kt
@@ -2,12 +2,12 @@ package com.noahjutz.findchip.ui.home
 
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.*
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Bluetooth
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.viewinterop.viewModel
+import com.noahjutz.findchip.R
 
 @Composable
 fun HomeScreen(
@@ -17,7 +17,7 @@ fun HomeScreen(
         topBar = {
             TopAppBar(
                 title = { Text("Findchip") },
-                navigationIcon = { IconButton(onClick = {}) { Icon(Icons.Default.Bluetooth) } }
+                navigationIcon = { IconButton(onClick = {}) { Icon(vectorResource(id = R.drawable.ic_launcher_foreground)) } }
             )
         }
     ) {
