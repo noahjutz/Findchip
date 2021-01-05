c713f80ac51084cece7a07e953e5b9ec3439b4e6
Noah Jutz <noahjutz@tutanota.de>
2021-01-05 21:42:27 +0100

    Show AlertDialog from FindchipApp

--- app/src/main/java/com/noahjutz/findchip/ui/entry_point/FindchipApp.kt
+++ app/src/main/java/com/noahjutz/findchip/ui/entry_point/FindchipApp.kt
@@ -1,9 +1,37 @@
 package com.noahjutz.findchip.ui.entry_point
 
+import androidx.compose.material.AlertDialog
+import androidx.compose.material.Button
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.viewinterop.viewModel
 import com.noahjutz.findchip.ui.NavGraph
 
 @Composable
-fun FindchipApp() {
-    NavGraph()
+fun FindchipApp(
+    viewModel: FindchipAppViewModel = viewModel()
+) {
+    Scaffold {
+        NavGraph()
+        val isBluetoothEnabled by viewModel.isBluetoothEnabled.collectAsState(initial = true)
+        if (!isBluetoothEnabled) {
+            AlertDialog(
+                onDismissRequest = {},
+                confirmButton = {
+                    Button(onClick = { /*TODO*/ }) {
+                        Text("Enable Bluetooth")
+                    }
+                },
+                title = {
+                    Text("Bluetooth is disabled.")
+                },
+                text = {
+                    Text("Bluetooth is required.")
+                },
+            )
+        }
+    }
 }
---
+++ app/src/main/java/com/noahjutz/findchip/ui/entry_point/FindchipAppViewModel.kt
@@ -0,0 +1,17 @@
+package com.noahjutz.findchip.ui.entry_point
+
+import android.bluetooth.BluetoothAdapter
+import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.flow
+
+class FindchipAppViewModel : ViewModel() {
+    private val bluetoothAdapter = BluetoothAdapter.getDefaultAdapter()
+
+    val isBluetoothEnabled = flow {
+        while (true) {
+            emit(bluetoothAdapter.isEnabled)
+            delay(500)
+        }
+    }
+}
