57eb9cc2dddcd3139c52410002dc7547f536801e
Noah Jutz <noahjutz@tutanota.de>
2021-01-06 17:54:06 +0100

    Remove HomeScreen composable

--- app/src/main/java/com/noahjutz/findchip/ui/NavGraph.kt
+++ app/src/main/java/com/noahjutz/findchip/ui/NavGraph.kt
@@ -6,17 +6,12 @@ import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.noahjutz.findchip.ui.device_details.DeviceDetails
 import com.noahjutz.findchip.ui.device_list.DeviceList
-import com.noahjutz.findchip.ui.home.HomeScreen
 
 @Composable
 fun NavGraph() {
     val navController = rememberNavController()
 
     NavHost(navController, startDestination = "deviceList") {
-        composable("home") {
-            HomeScreen()
-        }
-
         composable("deviceList") {
             DeviceList()
         }
--- app/src/main/java/com/noahjutz/findchip/ui/device_list/DeviceList.kt
+++ app/src/main/java/com/noahjutz/findchip/ui/device_list/DeviceList.kt
@@ -1,12 +1,27 @@
 package com.noahjutz.findchip.ui.device_list
 
+import androidx.compose.foundation.Image
+import androidx.compose.material.IconButton
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.viewinterop.viewModel
+import com.noahjutz.findchip.R
 
 @Composable
 fun DeviceList(
     viewModel: DeviceListViewModel = viewModel()
 ) {
-    Text("TODO Device list layout")
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text("DeviceList") },
+                navigationIcon = { IconButton(onClick = {}) { Image(vectorResource(id = R.drawable.ic_launcher_foreground)) } }
+            )
+        }
+    ) {
+        Text("TODO Device list layout")
+    }
 }
