0001c1e3f691b4ac4aab40cde98bae0cad494094
Noah Jutz <noahjutz@tutanota.de>
2021-01-06 18:59:26 +0100

    Reformat DeviceListViewModel

--- app/src/main/java/com/noahjutz/findchip/ui/device_list/DeviceListViewModel.kt
+++ app/src/main/java/com/noahjutz/findchip/ui/device_list/DeviceListViewModel.kt
@@ -6,7 +6,6 @@ import android.bluetooth.le.ScanCallback
 import android.bluetooth.le.ScanFilter
 import android.bluetooth.le.ScanResult
 import android.bluetooth.le.ScanSettings
-import android.util.Log
 import androidx.lifecycle.ViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
@@ -19,21 +18,15 @@ class DeviceListViewModel(
 
     init {
         bluetoothAdapter.bluetoothLeScanner.startScan(
-            listOf(ScanFilter.Builder().build()),
-            ScanSettings.Builder().build(),
+            listOf(ScanFilter.Builder().build()), // TODO
+            ScanSettings.Builder().build(), // TODO
             object : ScanCallback() {
                 override fun onScanResult(callbackType: Int, result: ScanResult?) {
                     super.onScanResult(callbackType, result)
-                    Log.i("DeviceListViewModel", "onScanResult: ($callbackType) $result")
                     result?.device?.let { device ->
                         when (callbackType) {
-                            ScanSettings.CALLBACK_TYPE_ALL_MATCHES -> {
-                                if (device !in devices.value) _devices.value += device
-
-                            }
-                            ScanSettings.CALLBACK_TYPE_MATCH_LOST -> {
-                                _devices.value -= device
-                            }
+                            ScanSettings.CALLBACK_TYPE_ALL_MATCHES -> if (device !in devices.value) _devices.value += device
+                            ScanSettings.CALLBACK_TYPE_MATCH_LOST -> _devices.value -= device
                         }
                     }
                 }
