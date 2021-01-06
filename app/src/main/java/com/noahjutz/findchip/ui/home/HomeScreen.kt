12ec83b1c10c262287f7f51e30e094fe82d48ccc
Noah Jutz <noahjutz@tutanota.de>
2021-01-06 15:29:41 +0100

    Add color to navigationIcon in topBar

--- app/src/main/java/com/noahjutz/findchip/ui/home/HomeScreen.kt
+++ app/src/main/java/com/noahjutz/findchip/ui/home/HomeScreen.kt
@@ -1,11 +1,11 @@
 package com.noahjutz.findchip.ui.home
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.ui.res.imageResource
 import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.viewinterop.viewModel
 import com.noahjutz.findchip.R
@@ -18,7 +18,7 @@ fun HomeScreen(
         topBar = {
             TopAppBar(
                 title = { Text("Findchip") },
-                navigationIcon = { IconButton(onClick = {}) { Icon(vectorResource(id = R.drawable.ic_launcher_foreground)) } }
+                navigationIcon = { IconButton(onClick = {}) { Image(vectorResource(id = R.drawable.ic_launcher_foreground)) } }
             )
         }
     ) {
