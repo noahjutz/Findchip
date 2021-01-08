254dce05677021364b1f1c1800751a908b42554a
Noah Jutz <noahjutz@tutanota.de>
2021-01-08 19:16:19 +0100

    Add button to start beeping

--- app/src/main/java/com/noahjutz/findchip/ui/device_details/DeviceDetails.kt
+++ app/src/main/java/com/noahjutz/findchip/ui/device_details/DeviceDetails.kt
@@ -1,13 +1,12 @@
 package com.noahjutz.findchip.ui.device_details
 
 import android.bluetooth.BluetoothDevice
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import org.koin.androidx.compose.getViewModel
 import org.koin.core.parameter.parametersOf
@@ -29,14 +28,22 @@ fun DeviceDetails(
         val isConnected by viewModel.isConnected.collectAsState(false)
         when {
             !isConnected -> ConnectingAlert(popBackStack)
-            else -> DeviceDetailsContent()
+            else -> DeviceDetailsContent(viewModel)
         }
     }
 }
 
 @Composable
-private fun DeviceDetailsContent() {
-    Text("Hello world!")
+private fun DeviceDetailsContent(
+    viewModel: DeviceDetailsViewModel
+) {
+    Column {
+        Button(onClick = {
+            viewModel.write()
+        }) {
+            Text("submit")
+        }
+    }
 }
 
 @Composable
--- app/src/main/java/com/noahjutz/findchip/ui/device_details/DeviceDetailsViewModel.kt
+++ app/src/main/java/com/noahjutz/findchip/ui/device_details/DeviceDetailsViewModel.kt
@@ -1,14 +1,15 @@
 package com.noahjutz.findchip.ui.device_details
 
 import android.app.Application
-import android.bluetooth.BluetoothDevice
-import android.bluetooth.BluetoothGatt
-import android.bluetooth.BluetoothGattCallback
-import android.bluetooth.BluetoothProfile
+import android.bluetooth.*
 import android.util.Log
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
+import java.util.*
 
 class DeviceDetailsViewModel(
     device: BluetoothDevice,
@@ -19,17 +20,64 @@ class DeviceDetailsViewModel(
             super.onConnectionStateChange(gatt, status, newState)
             _isConnected.value = newState == BluetoothProfile.STATE_CONNECTED
         }
+
+        override fun onServicesDiscovered(gatt: BluetoothGatt?, status: Int) {
+            super.onServicesDiscovered(gatt, status)
+            Log.d(
+                TAG,
+                "onServicesDiscovered: ${gatt?.services?.map { it.uuid }} ${gatt?.services?.map { it.characteristics.map { it.uuid } }}"
+            )
+        }
+    }
+
+    fun write() {
+        val serviceUUID = UUID.fromString("0000fff0-0000-1000-8000-00805f9b34fb")
+        val service: BluetoothGattService? = gatt.getService(serviceUUID)
+
+        val characteristicUUID = UUID.fromString("0000fff2-0000-1000-8000-00805f9b34fb")
+        val characteristic = service?.getCharacteristic(characteristicUUID)
+
+        characteristic?.let {
+            characteristic.writeType = BluetoothGattCharacteristic.WRITE_TYPE_NO_RESPONSE
+            characteristic.value = hexStringToByteArray("a301")
+                .also { Log.d(TAG, "byteArray: ${it.joinToString(",")}") }
+            gatt.writeCharacteristic(characteristic)
+        }
     }
 
     private val gatt: BluetoothGatt =
-        device.connectGatt(application.applicationContext, true, gattCallback).also { it.connect() }
+        device.connectGatt(application.applicationContext, true, gattCallback)
 
     private val _isConnected = MutableStateFlow(false)
     val isConnected = _isConnected.asStateFlow()
+
+    init {
+        viewModelScope.launch {
+            isConnected.collect {
+                if (!it) gatt.connect()
+                if (it) gatt.discoverServices()
+            }
+        }
+    }
 
     override fun onCleared() {
         super.onCleared()
         Log.d("DeviceDetailsVM", "onCleared")
         gatt.disconnect()
     }
+
+    companion object {
+        private const val TAG = "DeviceDetailsViewModel"
+    }
+}
+
+private fun hexStringToByteArray(input: String): ByteArray {
+    val data = ByteArray(input.length / 2)
+    var i = 0
+    while (i < input.length) {
+        data[i / 2] = ((Character.digit(input[i], 16) shl 4)
+                + Character.digit(input[i + 1], 16)).toByte()
+        i += 2
+    }
+    return data
 }
