32033b6a4c7cbfaf218af24cab8d19ecc24ad301
Noah Jutz <noahjutz@tutanota.de>
2021-01-09 17:53:24 +0100

    Better DeviceDetails layout
    Use FAB for beep toggle, add signal strength indicator, converting rssi
    to percent

--- app/src/main/java/com/noahjutz/findchip/ui/device_details/DeviceDetails.kt
+++ app/src/main/java/com/noahjutz/findchip/ui/device_details/DeviceDetails.kt
@@ -1,17 +1,25 @@
 package com.noahjutz.findchip.ui.device_details
 
 import android.bluetooth.BluetoothDevice
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.animation.animate
+import androidx.compose.animation.core.TweenSpec
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.VolumeOff
+import androidx.compose.material.icons.filled.VolumeUp
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import kotlinx.coroutines.flow.map
 import org.koin.androidx.compose.getViewModel
 import org.koin.core.parameter.parametersOf
+import kotlin.math.absoluteValue
 
 @Composable
 fun DeviceDetails(
@@ -24,6 +32,14 @@ fun DeviceDetails(
             TopAppBar(
                 navigationIcon = { IconButton(onClick = popBackStack) { Icon(Icons.Default.ArrowBack) } },
                 title = { Text("DeviceDetails") }
+            )
+        },
+        floatingActionButton = {
+            val isBeeping by viewModel.isBeeping.collectAsState()
+            ExtendedFloatingActionButton(
+                onClick = { if (isBeeping) viewModel.stopBeep() else viewModel.startBeep() },
+                icon = { Icon(if (isBeeping) Icons.Default.VolumeOff else Icons.Default.VolumeUp) },
+                text = { Text("${if (isBeeping) "Stop" else "Start"} beeping") },
             )
         }
     ) {
@@ -39,14 +55,24 @@ fun DeviceDetails(
 private fun DeviceDetailsContent(
     viewModel: DeviceDetailsViewModel
 ) {
-    Column {
-        val isBeeping by viewModel.isBeeping.collectAsState()
-        Button(onClick = { viewModel.toggleBeep() }) {
-            Text(if (isBeeping) "stop" else "start")
-        }
+    val signalStrength by viewModel.rssi.map { 100 - it.absoluteValue }.collectAsState(initial = 0)
+    Box(Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+        Column(horizontalAlignment = Alignment.CenterHorizontally) {
+            Text("Signal strength: $signalStrength%")
 
-        val rssi by viewModel.rssi.collectAsState()
-        Text(rssi.toString())
+            val color = MaterialTheme.colors.primary
+            val radius = animate((signalStrength * 3).toFloat(), animSpec = TweenSpec(2000))
+            Canvas(Modifier.preferredSize(300.dp)) {
+                drawCircle(
+                    color = color,
+                    radius = radius
+                )
+                drawCircle(
+                    color = color.copy(alpha = 0.1f),
+                    radius = 300f
+                )
+            }
+        }
     }
 }
 
